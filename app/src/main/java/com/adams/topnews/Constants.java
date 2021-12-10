d16f81410e9f05147e997edaae38bcba820403e2
echwaadams <adamsechwa18@gmail.com>
2021-12-10 17:17:39 +0300

    initialize String Constants

---
+++ app/src/main/java/com/adams/topnews/Constants.java
@@ -0,0 +1,6 @@
+package com.adams.topnews;
+
+public class Constants {
+    public static final String NEWSAPI_BASE_URL = "https://newsapi.org/v2/";
+    public static final String NEWS_API_KEY = BuildConfig.NEWS_API_KEY;
+}
