e3410a902624b03b93c098acba3c06731ea428b2
echwaadams <adamsechwa18@gmail.com>
2021-12-10 16:28:35 +0300

    implemented location search functionality.

--- app/src/main/java/com/adams/topnews/ui/LocationActivity.java
+++ app/src/main/java/com/adams/topnews/ui/LocationActivity.java
@@ -2,13 +2,47 @@ package com.adams.topnews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LocationActivity extends AppCompatActivity {
+import com.adams.topnews.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LocationActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    @BindView(R.id.locationTextView)
+    TextView mLocationTextView;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.findHomesButton)
+    Button mFindHomesButton;
+    @BindView(R.id.savedNewsButton) Button mSavedNewsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        //binding Views
+        ButterKnife.bind(this);
+
+        //setting clickListener
+        mFindHomesButton.setOnClickListener(this);
+        mSavedNewsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == mFindHomesButton){
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(LoginActivity.this, NewsActivity.class);
+            startActivity(intent);
+        }
     }
 }
