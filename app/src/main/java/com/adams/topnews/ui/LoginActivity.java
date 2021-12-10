a5973e663ea61ccccef03baaef3e03c438247114
echwaadams <adamsechwa18@gmail.com>
2021-12-10 16:17:34 +0300

    implemented login functionality

--- app/src/main/java/com/adams/topnews/ui/LoginActivity.java
+++ app/src/main/java/com/adams/topnews/ui/LoginActivity.java
@@ -2,13 +2,48 @@ package com.adams.topnews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.adams.topnews.R;
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.otherSignup)
+    TextView mOtherSignup;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+    @BindView(R.id.userNameEditText)
+    EditText mUserNameEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.loginButton)
+    MaterialButton mLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Binding Views
+        ButterKnife.bind(this);
+
+        //setting clickListener
+        mOtherSignup.setOnClickListener(this);
+
+        mLoginButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if (view == mOtherSignup){
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/adams/topnews/ui/SignUpActivity.java
+++ app/src/main/java/com/adams/topnews/ui/SignUpActivity.java
@@ -2,15 +2,68 @@ package com.adams.topnews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.adams.topnews.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+
+    //member variable
+    @BindView(R.id.otherSignin) TextView mOtherSignIn;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.signupTextView)
+    TextView mSignupTextView;
+    @BindView(R.id.signupButton)
+    Button mSignupButton;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.userNameEditText) EditText mUserNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //Binding views
+        ButterKnife.bind(this);
+
+        //setting clickListener
+        mSignupButton.setOnClickListener(this);
+        mOtherSignIn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if (view == mOtherSignIn){
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActionMode(intent);
+            finish();
+        }
+        //passing intent to login activity
+        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+        startActivity(intent);
+        //create new user
+        if (view == mSignupButton){
+            createNewUser();
+        }
+    }
+    //user creation
+    private void createNewUser(){
+        final String name = mUserNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
     }
 }
