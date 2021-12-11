e3607ac3c9a47e01164f379c8d5eace6f6414d4c
echwaadams <adamsechwa18@gmail.com>
2021-12-11 14:06:18 +0300

    add picasso images.

--- app/src/main/java/com/adams/topnews/adapters/NewsListAdapter.java
+++ app/src/main/java/com/adams/topnews/adapters/NewsListAdapter.java
@@ -1,7 +1,68 @@
 package com.adams.topnews.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.NewsViewHolder> {
+import com.adams.topnews.R;
+import com.adams.topnews.models.Article;
+import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.NewsViewHolder> {
+    private List<Article> mNews;
+    private Context mContext;
+
+    public NewsListAdapter(List<Article> mNews, Context mContext) {
+        this.mNews = mNews;
+        this.mContext = mContext;
+    }
+    @Override
+    public NewsListAdapter.NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_list_item, parent,false);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NewsListAdapter.NewsViewHolder holder, int position){
+        holder.bindNews(mNews.get(position));
+    }
+
+    @Override
+    public int getItemCount(){
+        return mNews.size();
+    }
+    public class NewsViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.newsImageView)
+        ImageView mNewsImageView;
+        @BindView(R.id.categoryTextView)
+        TextView mCategoryTextView;
+        @BindView(R.id.newsNameTextView) TextView mNewsNameTextView;
+        @BindView(R.id.recyclerCategory) RecyclerView mRecyclerCategory;
+
+
+        private Context mContext;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindNews(Article news) {
+            Picasso.get().load(news.getUrlToImage()).into(mNewsImageView);
+
+            mNewsNameTextView.setText(news.getAuthor());
+            mCategoryTextView.setText(news.getSource().getSystemId());
+        }
+    }
 }
--- app/src/main/java/com/adams/topnews/ui/NewsActivity.java
+++ app/src/main/java/com/adams/topnews/ui/NewsActivity.java
@@ -1,16 +1,101 @@
 package com.adams.topnews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.adams.topnews.R;
+import com.adams.topnews.adapters.NewsListAdapter;
+import com.adams.topnews.models.Article;
+import com.adams.topnews.models.News;
+import com.adams.topnews.models.Source;
+import com.adams.topnews.network.NewsApiInterface;
+import com.adams.topnews.network.NewsClient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewsActivity extends AppCompatActivity {
+    private static final String TAG = NewsActivity.class.getSimpleName();
+
+    @BindView(R.id.titleCategory)
+    TextView mTitleCategory;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.recyclerCategory)
+    RecyclerView mRecyclerCategory;
+
+    private NewsListAdapter mAdapter;
+
+    public List<Article> news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        //Binding views
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String country = intent.getStringExtra("country");
+
+
+        NewsApiInterface client = NewsClient.getNewsClient();
+
+        Call<News> call = client.getNews(country, "news");
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    news = response.body().getArticles();
+                    mAdapter = new NewsListAdapter(NewsActivity.this, news);
+                    mRecyclerCategory.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(NewsActivity.this);
+                    mRecyclerCategory.setLayoutManager(layoutManager);
+                    mRecyclerCategory.setHasFixedSize(true);
+
+                    showNews();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                hideProgressBar();
+                showFailureMessage();
+            }
+        });
+    }
+    private void showFailureMessage(){
+        mErrorTextView.setText("SOmething went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+    private void showUnsuccessfulMessage(){
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+    private void showNews(){
+        mRecyclerCategory.setVisibility(View.VISIBLE);
+    }
+    private void hideProgressBar(){
+        mProgressBar.setVisibility(View.GONE);
     }
 }
