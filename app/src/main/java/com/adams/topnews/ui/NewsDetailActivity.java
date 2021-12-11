7bbc8c5166e89001a8a49a5e8930180827425210
echwaadams <adamsechwa18@gmail.com>
2021-12-11 14:13:36 +0300

    created detail activity.

---
+++ app/src/main/java/com/adams/topnews/ui/NewsDetailActivity.java
@@ -0,0 +1,16 @@
+package com.adams.topnews.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adams.topnews.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
